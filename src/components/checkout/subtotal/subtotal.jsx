02069a7a400b40da46f7d36d1a93e4798da1f154
Vaishnav Nishanth A V <rsdevian.devbase@gmail.com>
2025-08-22 21:29:14 +0530

    Fixed package issue

--- src/components/checkout/subtotal/subtotal.jsx
+++ src/components/checkout/subtotal/subtotal.jsx
@@ -1,35 +1,28 @@
 import React from 'react'
-import CurrencyFormat from 'react-currency-format'
 import "./Subtotal.css"
 import { useStateValue } from '../../../context/stateProvider'
 import { getBasketTotal } from '../../../context/reducer';
 
 export default function Subtotal() {
-  const [ {basket}, dispatch ] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
+
+  // Create the currency formatter instance
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
 
   return (
-    <div  className='subtotal'>
-         <CurrencyFormat 
-            renderText={(value) => (
-              <>
-                <p>
-                    Subtotal ({basket.length} items):
-                    <strong>{value}</strong>
-                </p>
-                <small className='subtotal__gift'>
-                    <input type="checkbox" />
-                    This order contains a gift
-                </small>
-              </>
-            )}
-            decimalScale={2}
-            value={getBasketTotal(basket)}
-            displayType={"text"}
-            thousandSeperator={true}
-            prefix={"$"}
-         />
-
-         <button>Proceed to checkout</button>
+    <div className='subtotal'>
+      <p>
+        Subtotal ({basket.length} items):
+        <strong>{formatter.format(getBasketTotal(basket))}</strong>
+      </p>
+      <small className='subtotal__gift'>
+        <input type="checkbox" />
+        This order contains a gift
+      </small>
+      <button>Proceed to checkout</button>
     </div>
   )
 }
