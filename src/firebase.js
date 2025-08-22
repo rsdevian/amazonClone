cc6a3058ec55a5e9a4dc89faaa610cdf7282b326
Vaishnav Nishanth A V <rsdevian.devbase@gmail.com>
2025-08-22 21:52:05 +0530

    Added env variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnrk-BhHIAagUo-1f2OfYEamrXs9n1FHA",
-  authDomain: "challenge-ca542.firebaseapp.com",
-  projectId: "challenge-ca542",
-  storageBucket: "challenge-ca542.appspot.com",
-  messagingSenderId: "481957008783",
-  appId: "1:481957008783:web:cf4a28d0b5efdb5a00a7fa",
-  measurementId: "G-NF52V1G0B7"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
